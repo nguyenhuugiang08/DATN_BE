79abd61bbd51e4ffb13467b834ac90c980f75981
Nguyen Huu Giang <nguyenhuugiangtb08012001@gmail.com>
2022-11-26 17:01:47 +0700

    update orther controllers

--- src/app/controllers/othersController.js
+++ src/app/controllers/othersController.js
@@ -40,18 +40,18 @@ const othersController = {
             const flashSaleProducts = await Product.find({ discount: "50" }).limit(5);
 
             // get all products'category is "Đồ thể thao" and limit 5 documents
-            const sportsCategory = await Category.findOne({ name: "Đồ thể thao" }) ?? [];
+            const sportsCategory = await Category.findOne({ name: "Đồ thể thao" });
             let sportsProducts = [];
             if (sportsCategory) {
-                const products = await Product.find({ categoryId: sportsCategory._id }).limit(5);
+                const products = await Product.find({ categoryId: sportsCategory?._id }).limit(5);
                 sportsProducts = [...products];
             }
 
             // get all products'
-            const swimCategory = await Category.findOne({ name: "Đồ bơi" }) ?? [];
+            const swimCategory = await Category.findOne({ name: "Đồ bơi" });
             let swimProducts = [];
             if (swimCategory) {
-                const products = await Product.find({ categoryId: sportsCategory._id }).limit(5);
+                const products = await Product.find({ categoryId: swimCategory?._id }).limit(5);
                 swimProducts = [...products];
             }
 
@@ -59,29 +59,52 @@ const othersController = {
             const springCategory = await Category.findOne({ name: "Hoa cỏ mùa xuân" });
             let springProducts = [];
             if (springCategory) {
-                const products = await Product.find({ categoryId: springCategory._id }).limit(10);
+                const products = await Product.find({ categoryId: springCategory?._id }).limit(10);
                 springProducts = [...products];
             }
             // get product summer collection
             const summerCategory = await Category.findOne({ name: "Bộ sưu tập mùa hè" });
             let summerProducts = [];
             if (summerCategory) {
-                const products = await Product.find({ categoryId: summerCategory._id }).limit(10);
+                const products = await Product.find({ categoryId: summerCategory?._id }).limit(10);
                 summerProducts = [...products];
             }
             // get product winter collection
             const winterCategory = await Category.findOne({ name: "Hoa cỏ mùa đông" });
             let winterProducts = [];
             if (winterCategory) {
-                const products = await Product.find({ categoryId: winterCategory._id }).limit(10);
+                const products = await Product.find({ categoryId: winterCategory?._id }).limit(10);
                 winterProducts = [...products];
             }
 
             // get categories which are feature categories
-            const dressCategory = await Category.find({ name: "Đầm" });
-            const vestCategory = await Category.find({ name: "Vest-Blazer" }) ?? [];
-            const sleepCategory = await Category.find({ name: "Đồ ngủ" }) ?? [];
-            const shirtCategory = await Category.find({ name: "Áo sơ mi" }) ?? [];
+            const dressCategory = await Category.findOne({ name: "Đầm" });
+            let dressProducts = [];
+            if (dressCategory) {
+                const products = await Product.find({ categoryId: dressCategory?._id });
+                dressProducts = [...products];
+            }
+
+            const vestCategory = await Category.findOne({ name: "Vest-Blazer" });
+            let vestProducts = [];
+            if (vestCategory) {
+                const products = await Product.find({ categoryId: vestCategory?._id });
+                vestProducts = [...products];
+            }
+
+            const sleepCategory = await Category.findOne({ name: "Đồ ngủ" });
+            let sleepProducts = [];
+            if (sleepCategory) {
+                const products = await Product.find({ categoryId: sleepCategory?._id });
+                sleepProducts = [...products];
+            }
+
+            const shirtCategory = await Category.findOne({ name: "Vest-Blazer" });
+            let shirtProducts = [];
+            if (shirtCategory) {
+                const products = await Product.find({ categoryId: shirtCategory?._id });
+                shirtProducts = [...products];
+            }
 
             return res.status(200).json({
                 status: "Success",
@@ -90,37 +113,37 @@ const othersController = {
                     egaFashions: [
                         {
                             categoryName: "Đầm",
-                            number: dressCategory.length,
+                            number: dressProducts.length,
                             thumbnail:
                                 "https://bizweb.dktcdn.net/thumb/large/100/448/042/themes/876420/assets/season_coll_1_img.png?1666596948466",
                         },
                         {
                             categoryName: "Vest-Blazer",
-                            number: vestCategory.length,
+                            number: vestProducts.length,
                             thumbnail:
                                 "https://bizweb.dktcdn.net/thumb/large/100/448/042/themes/876420/assets/season_coll_2_img.png?1666596948466",
                         },
                         {
                             categoryName: "Đồ thể thao",
-                            number: sportsCategory.length,
+                            number: sportsProducts.length,
                             thumbnail:
                                 "https://bizweb.dktcdn.net/thumb/large/100/448/042/themes/876420/assets/season_coll_3_img.png?1666596948466",
                         },
                         {
                             categoryName: "Đồ bơi",
-                            number: swimCategory.length,
+                            number: swimProducts.length,
                             thumbnail:
                                 "https://bizweb.dktcdn.net/thumb/large/100/448/042/themes/876420/assets/season_coll_4_img.png?1666596948466",
                         },
                         {
                             categoryName: "Đồ ngủ",
-                            number: sleepCategory.length,
+                            number: sleepProducts.length,
                             thumbnail:
                                 "https://bizweb.dktcdn.net/thumb/large/100/448/042/themes/876420/assets/season_coll_5_img.png?1666596948466",
                         },
                         {
                             categoryName: "Áo sơ mi",
-                            number: shirtCategory.length,
+                            number: shirtProducts.length,
                             thumbnail:
                                 "https://bizweb.dktcdn.net/thumb/large/100/448/042/themes/876420/assets/season_coll_6_img.png?1666596948466",
                         },
