8968427e5f03304502c438eee779150170c5697f
Nguyen Huu Giang <nguyenhuugiangtb08012001@gmail.com>
2023-04-18 00:49:41 +0700

    update

--- src/app/controllers/othersController.js
+++ src/app/controllers/othersController.js
@@ -6,6 +6,29 @@ const Thumbnail = require("../../models/Thumbnail");
 const Size = require("../../models/Size");
 const Color = require("../../models/Color");
 
+const handlePopulateProduct = async (listResult, listInput) => {
+    for (let product of listInput) {
+        product = await product.populate("category", "name");
+
+        const thumbnails = await Thumbnail.find(
+            {
+                _id: { $in: product.thumbnails },
+            },
+            "url urlId -_id"
+        );
+
+        const sizes = await Size.find({
+            _id: { $in: product.sizes },
+        });
+        const colors = await Color.find({
+            _id: { $in: product.colors },
+        });
+        product = { ...product._doc, thumbnails, sizes, colors };
+        listResult.push(product);
+    }
+    return listResult;
+};
+
 const othersController = {
     search: asyncHandle(async (req, res, next) => {
         try {
@@ -35,33 +58,25 @@ const othersController = {
 
             // Lấy ra các sản phẩm thuộc danh mục áo nam
             const shirtProducts = await Product.find({ category: DB_RESOURCE.shirtCategory });
+            // Lấy ra các sản phẩm thuộc danh mục quần nam
+            const trousersProducts = await Product.find({ category: DB_RESOURCE.trousersCategory });
 
-            const listProducts = [];
-            for (let product of shirtProducts) {
-                product = await product.populate("category", "name");
+            let listshirtProducts = [];
+            listshirtProducts = await handlePopulateProduct(listshirtProducts, shirtProducts);
 
-                const thumbnails = await Thumbnail.find(
-                    {
-                        _id: { $in: product.thumbnails },
-                    },
-                    "url urlId -_id"
-                );
-
-                const sizes = await Size.find({
-                    _id: { $in: product.sizes },
-                });
-                const colors = await Color.find({
-                    _id: { $in: product.colors },
-                });
-                product = { ...product._doc, thumbnails, sizes, colors };
-                listProducts.push(product);
-            }
+            let listTrousersProducts = [];
+            listTrousersProducts = await handlePopulateProduct(
+                listTrousersProducts,
+                trousersProducts
+            );
 
             return res.status(200).json({
                 status: "Success",
                 data: {
                     bannerUrls,
-                    shirtProducts: listProducts,
+                    shirtProducts: listshirtProducts,
+                    trousersProducts: listTrousersProducts,
+                    homeBasic: DB_RESOURCE.HomeBasic,
                 },
             });
         } catch (error) {
--- src/base/resource.js
+++ src/base/resource.js
@@ -5,6 +5,11 @@ const DB_RESOURCE = {
         "https://media.coolmate.me/cdn-cgi/image/width=1920…y=90,format=auto/uploads/March2023/desktop_76.jpg",
     ],
     shirtCategory: "6433f115e20ab5121b937808",
+    trousersCategory: "6433f246e20ab5121b937809",
+    HomeBasic: {
+        home_basic_thumbnail: "https://mcdn.coolmate.me/image/April2023/mceclip0_49.jpg",
+        homepage_basic_content: "Combo 03 quần lót nam chỉ từ 99k",
+    },
 };
 
 module.exports = DB_RESOURCE;
